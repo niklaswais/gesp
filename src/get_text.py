1fcad5e1cfba76f1b1858451d2ac7905a769dfe3
nw <niklas.wais@uni-leipzig.de>
2022-09-08 14:50:30 +0200

    bugfix, empty page filter @ ni

--- src/get_text.py
+++ src/get_text.py
@@ -121,18 +121,23 @@ def mv(item, headers, cookies): # spider.headers, spider.cookies
 def ni(item):
     # Entscheidungstext bereinigen (kein Menü etc.)
     try:
-        tree = html.fromstring(requests.get(item["link"]).text)
+        txt = requests.get(item["link"]).text
     except:
-        output("could not parse " + item["link"], "err")
+        output("could not retrieve " + item["link"], "err")
     else:
-        if tree.xpath("//div[@id='bsentscheidung']"): # Herausfiltern von leeren Seiten
-            body_content = html.tostring(tree.xpath("//div[@class='jurisText']")[0]).decode("utf-8")
-            doc = "<html><head><title>%s</title></head><body>%s</body></html>" % (item["az"], body_content)
-            item["text"] = doc
-            item["filetype"] = "html"
-            return item
-        else:
-            output("empty page " + item["link"], "err")
+        try:
+            tree = html.fromstring(txt)
+        except:
+            output("could not parse " + item["link"], "err")
+        else:    
+            if txt.find("bsentscheidung") != -1: # Herausfiltern von leeren Seiten
+                body_content = html.tostring(tree.xpath("//div[@class='jurisText']")[0]).decode("utf-8")
+                doc = "<html><head><title>%s</title></head><body>%s</body></html>" % (item["az"], body_content)
+                item["text"] = doc
+                item["filetype"] = "html"
+                return item
+            else:
+                output("empty page " + item["link"], "err")
 
 def nw(item):
     try:
